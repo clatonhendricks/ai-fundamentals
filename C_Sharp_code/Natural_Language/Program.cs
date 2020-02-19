52cda28421629c9e5266e1d29be97b3613cbbc5e
Gerry O'Brien <gerry.obrien@microsoft.com>
2020-02-19 12:51:46 -0800

    Added C# project files

---
+++ C_Sharp_code/Natural_Language/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Natural_Language
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
